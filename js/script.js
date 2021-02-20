59b7c65979364ee2893196a8e6b702d1a6e0e08e
Lucas Axelson <lucasaxelson2016@GMAIL.COM>
2021-02-20 17:00:38 +0000

    Marker update

--- js/script.js
+++ js/script.js
@@ -11,15 +11,50 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map;
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
         function (position) {
             const { latitude } = position.coords
             const { longitude } = position.coords
             console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+
+            const coords = [latitude, longitude]
+
+            const map = L.map('map').setView(coords, 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            // Handling clicks on map
+            map.on('click', function (mapEvent) {
+                form.classList.remove(`hidden`)
+                inputDistance.focus()
+)
         },
         function () {
             alert(`Could not get your position`)
         })
 };
 
+form.addEventListener(`submit`, function () {
+    // Display marker 
+    console.log(mapEvent)
+    const { lat, lng } = mapEvent.latlng
+
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+        }))
+        .setPopupContent(`Workout`)
+        .openPopup();
+
+}
+})
